b4612e648f19b1a1b926e1a3f364cadadf2b1e50
zlq <356139089@qq.com>
2016-04-18 12:30:49 +0800

    update

--- prprAttack.py
+++ prprAttack.py
@@ -1,6 +1,47 @@
 # -*- coding:utf-8 -*-
 import urllib2
 import urllib
+import re
+import cookielib
+import os
+import requests
+
+
+def getimg(url):
+    request = urllib2.Request(url)
+    response = urllib2.urlopen(request)
+    return response.read()
+
+
+'''創建文件目录，并返回该目录'''
+def mkdir(path):
+    # 去除左右两边的空格
+    path=path.strip()
+    # 去除尾部 \符号
+    path=path.rstrip("\\")
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
+
+'''
+保存文件到本地
+
+@path  本地路径
+@file_name 文件名
+@data 文件内容
+'''
+def save_file(path, file_name, data):
+    if data == None:
+        return
+    mkdir(path)
+    if(not path.endswith("/")):
+        path=path+"/"
+    file=open(path+file_name, "wb")
+    file.write(data)
+    file.flush()
+    file.close()
+
+
 
 #定义一个要提交的数据数组(字典)
 Formdata = {}
@@ -17,11 +58,21 @@ user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
 url = 'http://prprleg.com/Mprpr/m'
 post_data = urllib.urlencode(Formdata)
 
-
-
 #提交，发送数据
 request = urllib2.Request(url, post_data)
 response = urllib2.urlopen(request)
 
 #获取提交后返回的信息
-print response.read()
+#print response.read()
+page = response.read()
+
+#使用正则表达式提取地址
+pattern = re.compile('{"id":(.*?),"url":"(.*?)","upload_time":".*?"}',re.S)
+result = re.findall(pattern,page)
+
+
+for i in result:
+    # print i[0]
+    # print i[1]
+    save_file ("F:/play/", i[0] + ".jpg", getimg("http://cdn.img.prprleg.com/" + i[1]))
+    print i[1] + "获取成功"
